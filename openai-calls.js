752f03e3cb94a050d31a4eb23b44e8ee66aa013a
propadiene-1 <aileenliang06@gmail.com>
2026-02-15 00:29:39 -0800

    syntax error skull

--- openai-calls.js
+++ openai-calls.js
@@ -319,7 +319,7 @@ async function analyzeTranscript(transcript) {
 module.exports = {
     generateFollowUpQuestion,
     autoScheduleFromTranscript,
-    extractKeywordsFromTranscript
+    extractKeywordsFromTranscript,
     extractDBColumns
     //analyzeTranscript
 };
